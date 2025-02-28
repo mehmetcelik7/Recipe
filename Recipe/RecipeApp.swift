d221f15c4ed576c71ac180da4df76d53f55a01dd
mehmet <mehmet.cel.ik@hotmail.com>
2025-02-28 13:22:13 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by mehmet Çelik on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
